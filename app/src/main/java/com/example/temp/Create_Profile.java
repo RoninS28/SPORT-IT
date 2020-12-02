5329274bbbf5f4c3710a7addf52a785672bb4392
Rohan Shiveshwarkar <60844691+RoninS28@users.noreply.github.com>
2020-12-02 13:35:36 +0530

    add auth, login functionality

--- app/src/main/java/com/example/temp/Create_Profile.java
+++ app/src/main/java/com/example/temp/Create_Profile.java
@@ -6,10 +6,15 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.DocumentReference;
@@ -21,6 +26,7 @@ public class Create_Profile extends AppCompatActivity implements View.OnClickLis
     FirebaseDatabase firebaseDatabase;
     DatabaseReference reference;
     FirebaseFirestore fdb;
+    FirebaseAuth auth;
 
     TextInputLayout contactDetails, email, password;
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,25 +52,43 @@ public class Create_Profile extends AppCompatActivity implements View.OnClickLis
         }
         else
         {
+//            boolean flag=false;
             String str_email = email.getEditText().getText().toString();
             String str_contactDetails = contactDetails.getEditText().getText().toString();
             String str_password = password.getEditText().getText().toString();
             PlayerData playerData = new PlayerData(str_email, str_contactDetails, str_password);
 
-            fdb = FirebaseFirestore.getInstance();
-            fdb.collection("Player_Profile").add(playerData).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            auth = FirebaseAuth.getInstance();
+            auth.createUserWithEmailAndPassword(str_email, str_password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                 @Override
-                public void onSuccess(DocumentReference documentReference) {
-                    Log.d("Success", "Document ID: " + documentReference.getId());
+                public void onComplete(@NonNull Task<AuthResult> task) {
+//                    flag = true;
+                        fdb = FirebaseFirestore.getInstance();
+            //            fdb.collection("Player_Profile").
+                    if(task.isSuccessful()){
+                        fdb.collection("Player_Profile").document(auth.getCurrentUser().getUid()).set(playerData).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Log.d("SUCCESS", "USER ADDED");
+                            }
+                        });
+
+
+                    }
+
                 }
             });
+//            if(flag == true){
+
+//            }
+
+
 
 //            firebaseDatabase = FirebaseDatabase.getInstance();
 //            reference = firebaseDatabase.getReference("Players");
 
 //            reference.setValue(playerData);
-            startActivity(new Intent(this, Login_Page.class));
-            this.finish();
+
         }
     }
 }
--- app/src/main/java/com/example/temp/Login_Page.java
+++ app/src/main/java/com/example/temp/Login_Page.java
@@ -1,37 +1,95 @@
 package com.example.temp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+//import com.google.api.core.ApiFuture;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-public class Login_Page extends AppCompatActivity implements View.OnClickListener {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.List;
+
+public class Login_Page extends AppCompatActivity  {
+    TextInputLayout iusername, ipassword;
+    FirebaseFirestore fdb;
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_page);
 
+        iusername = findViewById(R.id.username);
+        ipassword = findViewById(R.id.password);
+        auth = FirebaseAuth.getInstance();
+
         Button btnNewPlayer = findViewById(R.id.btnNewPlayer);
-        btnNewPlayer.setOnClickListener(this);
+//        btnNewPlayer.setOnClickListener(this);
         Button btnLogin = findViewById(R.id.btnGo);
-        btnLogin.setOnClickListener(this::onClick);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = iusername.getEditText().getText().toString();
+                String password = ipassword.getEditText().getText().toString();
+                Log.println(Log.INFO, "ENTERED USERNAME IS" + username, "USERNAME IS " + username);
+                Log.println(Log.INFO, "ENTERED PASSWORD IS" + password, "PASSWORD IS " + password);
+
+                auth.signInWithEmailAndPassword(username, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.println(Log.INFO, "SUCCESS" , "SUCCESS IS " );
+//                            startActivity(new Intent(getApplicationContext(), SinglePlayer.class));
+//                            finish();
+                        }
+
+                    }
+                });
+            }
+        });
     }
 
-    @Override
-    public void onClick(View v) {
-        if(((Button) v).getText().toString().equals("Login"))
-        {
-            // --! Add firebase auth for login
-            startActivity(new Intent(this, SinglePlayer.class));
-            this.finish();
-        }
-        else
-        {
-            startActivity(new Intent(this, Create_Profile.class));
-            this.finish();
-        }
-    }
+
+//    @Override
+//    public void onClick(View v) {
+////        if(((Button) v).getText().toString().equals("Login")) {
+////            boolean flag = false;
+////            String username = iusername.getEditText().getText().toString();
+////            String password = ipassword.getEditText().getText().toString();
+////            Log.println(Log.INFO, "ENTERED USERNAME IS" + username, "USERNAME IS " + username);
+////            Log.println(Log.INFO, "ENTERED PASSWORD IS" + password, "PASSWORD IS " + password);
+////            auth.signInWithEmailAndPassword(username, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+////                @Override
+////                public void onComplete(@NonNull Task<AuthResult> task) {
+////                    if(task.isSuccessful()){
+////                        startActivity(new Intent(getApplicationContext(), SinglePlayer.class));
+////                        finish();
+////                    }
+////
+////                }
+////            });
+////            // --! Add firebase auth for login
+////
+////
+////            }
+//
+//        if(((Button) v).getText().toString().equals("New Player?"))
+//        {
+//            startActivity(new Intent(this, Create_Profile.class));
+//            this.finish();
+//        }
+//    }
+
 }
